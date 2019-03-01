b1b93dcb61ff9506d7cbf730bf56c240b81ae4e7
vibhusehra <vibhusehra@gmail.com>
2019-03-01 11:37:05 +0530

    uploading image added to profile

--- app/src/main/java/com/example/vchat/AccountSetting.java
+++ app/src/main/java/com/example/vchat/AccountSetting.java
@@ -1,14 +1,21 @@
 package com.example.vchat;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.net.Uri;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
@@ -16,6 +23,12 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+import com.squareup.picasso.Picasso;
+import com.theartofdev.edmodo.cropper.CropImage;
+import com.theartofdev.edmodo.cropper.CropImageView;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 
@@ -30,16 +43,24 @@ public class AccountSetting extends AppCompatActivity {
     TextView statusTV,displayNameTV;
     CircleImageView profileImage;
 
+    ProgressDialog progressDialog;
+
+    StorageReference storageReference;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_account_setting);
+
+        progressDialog = new ProgressDialog(this);
 
         changeImage = findViewById(R.id.changeImage);
         changeStatus = findViewById(R.id.changeStatus);
         statusTV = findViewById(R.id.profileStatus);
         displayNameTV = findViewById(R.id.displayNameProfile);
         profileImage = findViewById(R.id.profileImageView);
+
+        storageReference = FirebaseStorage.getInstance().getReference();
 
         auth = FirebaseAuth.getInstance();
         user = auth.getCurrentUser();
@@ -57,6 +78,13 @@ public class AccountSetting extends AppCompatActivity {
                 displayNameTV.setText(name);
                 statusTV.setText(status);
 
+                //setting up the image
+                if(!image.equals("default")){
+                    //Picasso.with(SettingsActivity.this).load(image).into(mDisplayImage);
+                    Picasso.get().load(image).into(profileImage);
+                }
+
+
             }
 
             @Override
@@ -72,5 +100,86 @@ public class AccountSetting extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        changeImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                /*Intent galleryIntent = new Intent();
+                galleryIntent.setType("image/*");
+                galleryIntent.setAction(Intent.ACTION_GET_CONTENT);
+                startActivityForResult(Intent.createChooser(galleryIntent,"SELECT IMAGE"),1);*/
+
+                // start picker to get image for cropping and then use the image in cropping activity
+                CropImage.activity()
+                        .setGuidelines(CropImageView.Guidelines.ON)
+                        .start(AccountSetting.this);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == 1 && resultCode == RESULT_OK){
+            Uri imageUri = data.getData();
+
+            CropImage.activity(imageUri)
+                    .setAspectRatio(1,1)
+                    .start(this);
+        }
+        if (requestCode == CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE) {
+            CropImage.ActivityResult result = CropImage.getActivityResult(data);
+            if (resultCode == RESULT_OK) {
+                Uri resultUri = result.getUri();
+
+                progressDialog.setTitle("Please Wait");
+                progressDialog.setMessage("Uploading Image");
+                progressDialog.setCanceledOnTouchOutside(false);
+                progressDialog.show();
+
+                String uid = user.getUid();
+                final StorageReference filePath = storageReference.child("profile_images").child(uid + ".jpg");
+
+                filePath.putFile(resultUri).addOnCompleteListener(new OnCompleteListener<UploadTask.TaskSnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<UploadTask.TaskSnapshot> task) {
+                        if(task.isSuccessful()){
+                            //Toast.makeText(AccountSetting.this,"Success",Toast.LENGTH_SHORT).show();
+                            //String dowloadImage = task.getResult().
+                            //String downloadImage = task.getResult().getMetadata().getReference().getDownloadUrl().toString();
+                            filePath.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                                @Override
+                                public void onSuccess(Uri uri) {
+                                    final String downloadImage = uri.toString();
+                                    databaseReference.child("image").setValue(downloadImage).addOnCompleteListener(new OnCompleteListener<Void>() {
+                                        @Override
+                                        public void onComplete(@NonNull Task<Void> task) {
+                                            if(task.isSuccessful()){
+                                                progressDialog.dismiss();
+                                                Toast.makeText(AccountSetting.this,"Image upload Successful",Toast.LENGTH_SHORT).show();
+                                            }
+                                            else {
+                                                Toast.makeText(AccountSetting.this,"Failure",Toast.LENGTH_SHORT).show();
+                                                progressDialog.dismiss();
+                                            }
+                                        }
+                                    });
+                                }
+                            });
+
+
+                        }
+                        else {
+                            Toast.makeText(AccountSetting.this,"Falied to upload image",Toast.LENGTH_SHORT).show();
+                            progressDialog.dismiss();
+                        }
+
+                    }
+                });
+
+            } else if (resultCode == CropImage.CROP_IMAGE_ACTIVITY_RESULT_ERROR_CODE) {
+                Exception error = result.getError();
+            }
+        }
     }
 }
